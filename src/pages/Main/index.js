c98cff4d54e084a63d3efaa00465b19b5fe8a213
KirillIvano <ivannadez@yandex.ru>
2020-02-22 23:21:08 +0300

    minor fixes

---
+++ src/pages/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import {About} from '@/parts/About';
+import {HeadlinePicture} from '@/parts/HeadlinePicture';
+
+export const Main = () => (
+    <>
+        <HeadlinePicture />
+        <About />
+    </>
+);
+
