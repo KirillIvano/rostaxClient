d38dc815c24085f97ec5108730b202996a8faf19
KirillIvano <ivannadez@yandex.ru>
2020-02-22 13:57:48 +0300

    update landing

--- src/parts/About/index.js
+++ src/parts/About/index.js
@@ -12,30 +12,33 @@ export const About = () => {
             </h1>
             <div className={styles.aboutItems}>
                 <div className={styles.item}>
+                    <img className={styles.itemView} src={require('./../../images/thumb-up.svg')} alt="" />
                     <h1 className={styles.itemHeadline}>
                         КАЧЕСТВО
                     </h1>
                     <p className={styles.itemContent}>
                         {'Компания постоянно совершенствует технологию производства,\
-                            благодаря чему достигается качество выпускаемой продукции.'}
+                            благодаря чему достигается качество выпускаемой продукции'}
                     </p>
                 </div>
                 <div className={styles.item}>
+                    <img className={styles.itemView} src={require('./../../images/assortiment.svg')} alt="" />
                     <h1 className={styles.itemHeadline}>
-                        КАЧЕСТВО
+                        АССОРТИМЕНТ
                     </h1>
                     <p className={styles.itemContent}>
-                        {'Компания постоянно совершенствует технологию производства,\
-                            благодаря чему достигается качество выпускаемой продукции.'}
+                        {'Мы имеем широкий ассортимент различных типов продукции'}
                     </p>
                 </div>
                 <div className={styles.item}>
+                    <span className={styles.experience}>
+                        1994
+                    </span>
                     <h1 className={styles.itemHeadline}>
-                        КАЧЕСТВО
+                        ОПЫТ
                     </h1>
                     <p className={styles.itemContent}>
-                        {'Компания постоянно совершенствует технологию производства,\
-                            благодаря чему достигается качество выпускаемой продукции.'}
+                        {'Мы занимаемся производством с 1994 года, и имеем широкий опыт в этом деле'}
                     </p>
                 </div>
             </div>
